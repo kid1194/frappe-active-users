2498fc7d1983e988253747b629bf514d39b2fdd3
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-24 22:44:50 +0300

    Alpha release

---
+++ active_users/active_users/doctype/active_users_has_user/active_users_has_user.py
@@ -0,0 +1,9 @@
+# Active Users © 2022
+# Author:  Ameen Ahmed
+# Company: Level Up Marketing & Software Development Services
+# Licence: Please refer to license.txt
+
+from frappe.model.document import Document
+
+class ActiveUsersHasUser(Document):
+    pass
