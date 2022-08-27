6e550825d953a28522f7f8245ba6c49ac0ad84ff
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-27 18:25:13 +0300

    Alpha release

--- active_users/active_users/doctype/active_users_settings/test_active_users_settings.py
+++
@@ -1,9 +0,0 @@
-# Active Users © 2022
-# Author:  Ameen Ahmed
-# Company: Level Up Marketing & Software Development Services
-# Licence: Please refer to license.txt
-
-import unittest
-
-class TestActiveUsersSettings(unittest.TestCase):
-	pass
